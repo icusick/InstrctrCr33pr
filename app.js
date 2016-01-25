b421bc7000e6d38d53c9227b80622741de300053
isabel cusick <cusick.isabel@gmail.com>
2016-01-25 00:28:30 -0500

    basic functionality

--- app.js
+++ app.js
@@ -1,28 +1,53 @@
 var express = require('express');
 var app = express();
-// var MongoClient = require('mongodb').MongoClient;
+var MongoClient = require('mongodb').MongoClient;
 var bodyParser = require('body-parser');
 
 // Config
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs')
 
-// // db
-// var db;
-// var MongoClient = require('mongodb').MongoClient;
-// var ObjectId = require('mongodb').ObjectId;
-// var mongoUrl = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/instrctr_app_dev';
-// MongoClient.connect(mongoUrl, function(err, database) {
-//   if (err) { throw err; }
-//   db = database;
-//   process.on('exit', db.close);
-// });
+// db
+var db;
+var MongoClient = require('mongodb').MongoClient;
+var ObjectId = require('mongodb').ObjectId;
+var mongoUrl = process.env.MONGOLAB_URI || 'mongodb://localhost:27017/instrctr_app_dev';
+MongoClient.connect(mongoUrl, function(err, database) {
+  if (err) { throw err; }
+  db = database;
+  process.on('exit', db.close);
+});
 
 // Routes
-app.get('/', function(req, res) {
+app.get('/', function(req, res){
 	res.render('index')
 })
+
+app.get('/sung', function(req, res){
+	db.collection('sung').find({}).toArray(function(err, result) {
+  	console.log(result)
+  	res.json(result);
+  })
+})
+
+app.get('/anna', function(req, res){
+	db.collection('anna').find({}).toArray(function(err, result) {
+  	console.log(result)
+  	res.json(result);
+  })
+})
+
+app.get('/phil', function(req, res){
+	db.collection('phil').find({}).toArray(function(err, result) {
+		res.json(result)
+	})
+})
+
+
+
+
+
 
 
 app.listen(process.env.PORT || 3000);
---
+++ public/main.js
@@ -0,0 +1,83 @@
+console.log('linked');
+
+var renderSung = function(response){
+	var raw = $('#sung-temp').html()
+	var template = Handlebars.compile(raw);
+	console.log(response)
+	$sungDiv = $('<div>', {id: 'sungID'})
+	response.forEach(function(item){
+		console.log(item)
+		$sungDiv.append(template(item))
+	})
+	$('body').append($sungDiv)
+};
+
+var renderAnna = function(response){
+	
+	var raw = $('#anna-temp').html()
+	var template = Handlebars.compile(raw); 
+	$annaDiv = $('<div>', {id: 'annaID'})
+	response.forEach(function(item){
+		$annaDiv.append(template(item))
+	})
+	$('body').append($annaDiv)
+}
+
+var renderPhil = function(response){
+	console.log(response)
+	var raw = $('#phil-temp').html()
+	var template = Handlebars.compile(raw)
+	$philDiv = $('<div>', {id: 'philID'})
+	response.forEach(function(item) {
+		$philDiv.append(template(item))
+	})
+	$('body').append($philDiv)
+}
+
+
+
+$(document).ready(function() {
+	$('#sung').on('click', function() {
+		if (document.contains(document.getElementById("annaID"))) {
+			document.getElementById("annaID").remove();
+		} else if (document.contains(document.getElementById("philID"))) {
+			document.getElementById("philID").remove();
+		}
+		$('#who').remove()
+		$.ajax({
+			url: '/sung', 
+			type: 'GET', 
+			dataType: 'JSON'
+		}).done(renderSung)
+	});
+
+	$('#anna').on('click', function() {
+		if (document.contains(document.getElementById("sungID"))) {
+			document.getElementById("sungID").remove();
+		} else if (document.contains(document.getElementById("philID"))) {
+			document.getElementById("philID").remove();
+		}
+		$('#who').remove()
+		$.ajax({
+			url: '/anna', 
+			type: 'GET', 
+			dataType: 'JSON'
+		}).done(renderAnna)
+	});
+
+	$('#phil').on('click', function() {
+		if (document.contains(document.getElementById("sungID"))) {
+			document.getElementById("sungID").remove();
+		} else if (document.contains(document.getElementById("annaID"))) {
+			document.getElementById("annaID").remove();
+		}
+		$('#who').remove()
+		$.ajax({
+			url: '/phil', 
+			type: 'GET', 
+			dataType: 'JSON'
+		}).done(renderPhil)
+	})
+
+
+});
